149a77a118ae655e01934cbc1efe01979904ffdf
Hassaan Uddin Sheikh <unbundleracers@gmail.com>
2022-07-01 21:14:12 +0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul className="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/logIn">LogIn</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/logIn" element={<LogIn />} />
+      </Routes>
+    </Router>
   );
 }
 
